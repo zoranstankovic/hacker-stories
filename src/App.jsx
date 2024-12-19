4009dfc97af52f5a85b84e862d5f6acb8d5f14a5
Zoran Stankovic <zoran@Zorans-MacBook-Pro.local>
2024-12-19 20:58:51 +0100

    Add prettierrc config and extract code to new components
    Add .prettierrc config file, formatted code with prettier. Extracted
    code to new components Search and List.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 const list = [
   {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
@@ -22,25 +22,41 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return <li key={item.objectID}>
-            <span>
-            <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
 
-export default App
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
